e53269e56cc33cdf6304ee461062297c28a9b3c4
Hollajay <127396029+Hollajay@users.noreply.github.com>
2024-06-26 02:21:03 +0100

    correction in my about page

--- src/component/portfolio_page/about_page/About.jsx
+++ src/component/portfolio_page/about_page/About.jsx
@@ -4,7 +4,10 @@ export const About = () => {
   return (
     <section className='aboutMe_container' id='aboutPage' >
       <div className='left_about_grid'>
-        <img src="https://images.unsplash.com/photo-1453396450673-3fe83d2db2c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydGZvbGltZSUyMG1hbiUyMHBob3RvJTIwaW1hZ2V8ZW58MHx8MHx8fDA%3D" alt="image" />
+        <div className='img-container'>
+        <img src="https://i.postimg.cc/SKptkTt8/profileimage.jpg" alt="image" />
+        </div>
+    
       </div>
       <div className='Right_about_container'>
         <h1 className='Right_about_header'>Know me more</h1>
